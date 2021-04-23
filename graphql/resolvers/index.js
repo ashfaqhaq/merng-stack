d00c7ff61e83dca93f88a0c0a4e2dee1d0656ba7
iftekhar ul haq <ashfaq.28010@gmail.com>
2021-04-24 02:27:40 +0530

    add mutations for index file

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -5,6 +5,10 @@ const userResolvers = require("./users");
 module.exports = {
     Query:{
         ...postResolvers.Query
+    },
+    Mutation:{
+        ...userResolvers.Mutation,
+        // ...
     }
-}
+};
 
