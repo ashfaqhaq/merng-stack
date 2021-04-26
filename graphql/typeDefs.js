36bcedebee9dc972e745330cbc413b9a53a6dd97
iftekhar ul haq <ashfaq.28010@gmail.com>
2021-04-26 17:41:53 +0530

    CRUD comment, like  and Counts

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -5,8 +5,13 @@ type Post{
   id:ID!,
   body:String!,
   createdAt : String!,
-  username:String
+  username:String!
+  comments: [Comment]!
+    likes: [Like]!
+    likeCount: Int!
+    commentCount: Int!
 }
+
   type Query{
      getPosts : [Post] 
      getPost(postId:ID!) : Post
@@ -25,10 +30,24 @@ type User{
     password: String!
     confirmPassword: String!
   }
+  type Comment{
+    id:ID!
+    username:String!
+    body:String!
+  }
+  type Like{
+    id:ID!
+    username:String!
+    createdAt: String!
+  }
+
 type Mutation{
   register(registerInput: RegisterInput): User!
   login(username: String!, password:String!): User!
   createPost(body: String!): Post!
   deletePost(postId: ID!): String!
+  createComment(postId:ID!,body:String!): Comment!
+  deleteComment(postId:ID!,commentId:ID!): Post!
+  likePost(postId:ID!):Post!
 }
 `;
