283a0431dcd3a68d03f91850cd765813bb63f84d
iftekhar ul haq <ashfaq.28010@gmail.com>
2021-05-01 00:27:18 +0530

    first commit of react

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,12 @@
+// deprecated file, doesn't work
+
+
+// import React from 'react';
+// import App from './App';
+
+
+// export default (
+//   <ApolloProvider client={client}>
+//     <App />
+//   </ApolloProvider>
+// );
